841319e5502ea3b6ba3c6993aa522825e5e471d7
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-11-14 14:13:59 -0500

    Support Python 3.7
    Adds Python 3.7 to the CI matrix and begins testing it.

--- setup.py
+++ setup.py
@@ -148,9 +148,9 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     tests_require=test_requirements
 )
