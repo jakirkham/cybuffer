7b5c89ff4236cd796a5c6302933cfb6b1df7d590
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-11-19 22:54:30 -0500

    Add some tests of cybuffer
    Makes sure that `cybuffer` is able to handle some common cases
    equivalently on Python 2/3. Compare against Python 2/3's `memoryview` to
    make sure that the various features work equivalently. In cases where
    Python 2 lacks the added features or deviates, check that it matches
    Python 3's expected behavior on Python 2.

--- tests/test_cybuffer.py
+++ tests/test_cybuffer.py
@@ -3,11 +3,173 @@
 
 from __future__ import absolute_import
 
+import array
+import contextlib
+import mmap
+import sys
+
 import pytest
 
+from cybuffer import cybuffer
 
-def test_import_toplevel():
-    try:
-        import cybuffer
-    except ImportError:
-        pytest.fail("Unable to import `cybuffer`.")
+
+try:
+    buffer
+except NameError:
+    buffer = memoryview
+
+
+def test_empty_constructor():
+    with pytest.raises(TypeError):
+        b = cybuffer()
+
+
+def validate_against_memoryview(v, b, m, suboffsets=tuple()):
+    # Test view properties' data relationships
+    assert b.obj is v
+    assert b.nbytes == len(m.tobytes())
+    assert b.itemsize == (len(m.tobytes()) // len(v))
+    assert b.ndim == m.ndim
+    assert b.suboffsets == suboffsets
+    assert b.shape == (len(v),)
+    assert b.strides == (len(m.tobytes()) // len(v),)
+
+    # Test Python 3+ properties
+    if sys.version_info.major > 2:
+        assert b.obj is m.obj
+        assert b.c_contiguous == m.c_contiguous
+        assert b.f_contiguous == m.f_contiguous
+        assert b.contiguous == m.contiguous
+        assert b.nbytes == m.nbytes
+
+    # Test methods
+    assert b.tobytes() == m.tobytes()
+
+
+@pytest.mark.parametrize("v", [
+    b"abcdefghi",
+    bytearray(b"abcdefghi"),
+])
+def test_bytes(v):
+    # Initialize buffers
+    b = cybuffer(v)
+    m = memoryview(v)
+
+    # Validate format
+    assert b.format == m.format
+    assert b.itemsize == m.itemsize
+
+    # Validate contiguity
+    assert b.c_contiguous
+    assert b.f_contiguous
+    assert b.contiguous
+
+    # Validate permissions
+    assert b.readonly == m.readonly
+
+    # Test methods
+    assert b.tolist() == m.tolist()
+
+    validate_against_memoryview(v, b, m)
+
+
+@pytest.mark.parametrize("f",
+    ["b", "B", "h", "H", "i", "I", "l", "L", "q", "Q", "f", "d"]
+)
+def test_1d_arrays(f):
+    # Skip some newer types
+    if sys.version_info.major < 3 and f in "qQ":
+        pytest.skip("Format `%s` not available on Python 2" % f)
+
+    # Initialize buffers
+    v = array.array(f, [0, 1, 2, 3, 4])
+    b = cybuffer(v)
+    m = memoryview(buffer(v))
+
+    # Validate format
+    assert b.format == v.typecode
+    assert b.itemsize == v.itemsize
+
+    # Validate contiguity
+    assert b.c_contiguous
+    assert b.f_contiguous
+    assert b.contiguous
+
+    # Validate permissions
+    if isinstance(b, memoryview):
+        assert b.readonly
+    else:
+        assert not b.readonly
+
+    # Test methods
+    assert b.tolist() == v.tolist()
+
+    validate_against_memoryview(v, b, m)
+
+
+@pytest.mark.parametrize("f, s", [
+    ("c", b"Hello World!"),
+    ("u", u"Hello World!"),
+])
+def test_1d_text_arrays(f, s):
+    # Skip some newer types
+    if sys.version_info.major > 2 and f is "c":
+        pytest.skip("Format `%s` not available on Python 3" % f)
+
+    # Initialize buffers
+    v = array.array(f, s)
+    b = cybuffer(v)
+    m = memoryview(buffer(v))
+
+    # Validate format
+    assert b.itemsize == v.itemsize
+    if f is "u" and sys.maxunicode < 65536:
+        assert b.format == "H"
+    elif f is "u" and sys.maxunicode >= 65536:
+        assert b.format == "I"
+    elif f is "c":
+        assert b.format == "B"
+
+    # Validate contiguity
+    assert b.c_contiguous
+    assert b.f_contiguous
+    assert b.contiguous
+
+    # Validate permissions
+    if isinstance(b, memoryview):
+        assert b.readonly
+    else:
+        assert not b.readonly
+
+    # Test methods
+    assert b.tolist() == list(map(ord, v))
+
+    validate_against_memoryview(v, b, m)
+
+
+def test_mmap():
+    with contextlib.closing(mmap.mmap(-1, 10, prot=mmap.PROT_WRITE)) as v:
+        # Initialize buffers
+        b = cybuffer(v)
+        m = memoryview(buffer(v))
+
+        # Validate format
+        assert b.format == m.format
+        assert b.itemsize == m.itemsize
+
+        # Validate contiguity
+        assert b.c_contiguous
+        assert b.f_contiguous
+        assert b.contiguous
+
+        # Validate permissions
+        assert not b.readonly
+
+        # Test methods
+        assert b.tolist() == m.tolist()
+
+        validate_against_memoryview(v, b, m)
+
+        # Cleanup to close memory
+        del b
+        del m
